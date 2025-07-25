f8da47e2ccb4e29b0b314eb3eaa28c3bd13c35fe
Edwin420s <eduedwyn5@gmail.com>
2025-07-25 13:32:01 +0300

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,8 @@
 // src/services/api.js
 import axios from 'axios';
 
-// Create axios instance with proper baseURL
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
@@ -11,7 +10,6 @@ const api = axios.create({
   }
 });
 
-// Add response interceptor
 api.interceptors.response.use(
   response => response,
   error => {
